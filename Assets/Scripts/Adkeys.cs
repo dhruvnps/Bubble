0c9fc5fd2840042974081f5563a293bbc58dad6d
Dhruv Raja <59893449+dhruvnps@users.noreply.github.com>
2021-06-25 23:14:16 +0100

    Create Adkeys.cs

---
+++ Assets/Scripts/Adkeys.cs
@@ -0,0 +1,6 @@
+public class Adkeys
+{
+    // test ad keys
+    static public string adUnitId_IPHONE = "ca-app-pub-3940256099942544/4411468910";
+    static public string adUnitId_ANDROID = "ca-app-pub-3940256099942544/1033173712";
+}
